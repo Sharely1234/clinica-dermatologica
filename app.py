83e397cb43ca7141b90315eaf36b3063268e5ac4
Sharely1234 <135617935+Sharely1234@users.noreply.github.com>
2025-08-31 14:12:31 -0500

    Agregando Procfile y runtime.txt para despliegue

--- app.py
+++ app.py
@@ -307,4 +307,6 @@ def guardar_resultado():
 
 # Ejecutar app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
